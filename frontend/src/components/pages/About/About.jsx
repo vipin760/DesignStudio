afc5ef945a0b9317a5bea6cd11412fb733584db4
sreeshma <sreeshma.t.2019@gmail.com>
2024-06-12 00:24:22 +0530

    updated

--- frontend/src/components/pages/About/About.jsx
+++ frontend/src/components/pages/About/About.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      about page works
-    </div>
-  )
-}
+    return <div>about page...........</div>;
+};
 
-export default About
+export default About;
